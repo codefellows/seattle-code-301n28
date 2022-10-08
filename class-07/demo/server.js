c226cd779053f961dba9a37532f6a44d87dc227b
Hexx King <hexxking13@gmail.com>
2022-10-08 11:55:58 -0700

    adds server demo

---
+++ class-07/demo/server.js
@@ -0,0 +1,59 @@
+'use strict';
+
+// requires are similar to imports
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const lists = require('./data/shopping-list.json');
+
+// create an instance of an Express server
+const app = express();
+
+// middleware - tells our express app to use cors
+app.use(cors());
+
+// set our PORT variable to tell our Express app where to serve our server
+// PORT is NOT bananas! It must be named exactly this, because Heroku looks for a variable named PORT
+const PORT = process.env.PORT || 3002;
+
+// define the "home" route aka endpoint
+app.get('/', (request, response) => {
+  response.send('testing, testing...is this thing on?');
+});
+
+// define an endpoint that gets the shoppingList data and returns it to React
+app.get('/shopping-list', (req, res) => {
+  // grab the searchQuery from the request object 
+  // notice that the query parameter is named "type"
+  // "type" is the name of query parameter we must send along with Axios from React in order to ask for data from our server
+  const type = req.query.bananas;
+  console.log('query parameter: ', req.query);
+  console.log('type: ', type);
+  const shoppingList = new List(type);
+  const listItems = shoppingList.getItems();
+  res.status(200).send(listItems);
+});
+
+class List {
+  constructor(type){
+    // find method to find the type of list we want to return
+    let { listName, items } = lists.lists.find(list => list.listName === type);
+    // console.log(lists.lists);
+    this.type = listName;
+    this.itemValues = items;
+  }
+
+  // a method that gets just the name and desc properties from our item objects in the itemValues array
+  getItems() {
+    return this.itemValues.map(item => ({
+      name: item.name,
+      description: item.description
+    }));
+  }
+
+}
+
+// this line of code needs to be the LAST line in the file
+// listen tells our app which port to listen on
+// which port to serve our server on
+app.listen(PORT, console.log(`listening on PORT ${PORT}`));
